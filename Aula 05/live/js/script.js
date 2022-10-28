10861c50a3e4a15ba7307ae7be055d9fae06b119
Harison Herman Silva <harison.silva@cenergel.com.br>
2022-10-28 10:03:59 -0300

    Aula 05

--- Aula 05/live/js/script.js
+++ Aula 05/live/js/script.js
@@ -1,78 +1,88 @@
 let contAtivLogado = sessionStorage.getItem("contAtivLogado")
 let logado = JSON.parse(contAtivLogado)
 
-if(logado !== true){
+if (logado !== true) {
     window.location = "login.html"
 }
 
 let btnLogout = document.querySelector("#BtnLogout")
-btnLogout.addEventListener("click", ()=>{
+btnLogout.addEventListener("click", () => {
     sessionStorage.setItem("contAtivLogado", false)
     window.location = "login.html"
 })
 
-function obtemValor(){
+function obtemValor() {
     let inicio = document.querySelector("#inicio").value
     let fim = document.querySelector("#fim").value
     let atividade = document.querySelector("#atividade").value
-    console.table({inicio, fim, atividade})
-    if(verificaValidade(inicio) && 
-            verificaValidade(fim) &&
-            verificaValidade(atividade)){                
-                let obj = {
-                    inicio, 
-                    fim, 
-                    atividade,
-                    "minutos": calculaTempo(fim, inicio)
-                }
-                return obj
-            }
+    console.table({ inicio, fim, atividade })
+    if (verificaValidade(inicio) &&
+        verificaValidade(fim) &&
+        verificaValidade(atividade)) {
+        let obj = {
+            inicio,
+            fim,
+            atividade,
+            "minutos": calculaTempo(fim, inicio)
+        }
+        return obj
+    }
+    else {
+        return null
+    }
 }
 
-const verificaValidade = function (campo){
-    if(campo === ''){
+const verificaValidade = function (campo) {
+    if (campo === '') {
         alert("Informe todos os campos")
         return false
-    }else{
+    } else {
         return true
     }
 }
 
-let atividades = obtemAtividadesStorage()
-inicializaDOM()
+let atividades = obtemAtividadesStorage("atividades")
+let historico = obtemAtividadesStorage("historico")
+inicializaDOM(atividades, "#listaAtividades")
+inicializaDOM(historico, "#atividadesConfirmadas")
 
-function inicializaDOM(){
-    if(atividades.length > 0){
-        for(let atividade of atividades){
-            insereNoDOM(atividade)
+function inicializaDOM(lista, idTabDOM) {
+    if (lista.length > 0) {
+        for (let atividade of lista) {
+            insereNoDOM(atividade, idTabDOM)
         }
     }
 }
 
-function atualizaAtividadesStorage(){
+function atualizaAtividadesStorage() {
     localStorage.setItem("atividades", JSON.stringify(atividades))
+    localStorage.setItem("historico", JSON.stringify(historico))
 }
 
-function obtemAtividadesStorage(){
-    let aux = localStorage.getItem("atividades")
-    if(aux){
+function obtemAtividadesStorage(chave) {
+    let aux = localStorage.getItem(chave)
+    if (aux) {
         return JSON.parse(aux)
-    }else{
+    } else {
         return Array()
     }
 }
 
-let formulario = document.querySelector("form")
-formulario.addEventListener("submit", (evento)=>{
+const evtSubmit = (evento) => {
     evento.preventDefault()
     let obj = obtemValor()
-    formulario.reset()
-    if(insereNaLista(atividades, obj)){
-        insereNoDOM(obj)
+    if (obj) {
+        formulario.reset()
+        if (insereNaLista(atividades, obj)) {
+            insereNoDOM(obj, "#listaAtividades")
+        }
     }
-})
+}
 
-function calculaTempo(fim, inicio){
+let formulario = document.querySelector("form")
+formulario.addEventListener("submit", evtSubmit)
+
+function calculaTempo(fim, inicio) {
     let vetorFim = fim.split(':')
     let vetorInicio = inicio.split(':')
     let fimMinutos = parseInt(vetorFim[0]) * 60 + parseInt(vetorFim[1])
@@ -81,22 +91,22 @@ function calculaTempo(fim, inicio){
     return tempo
 }
 
-function sãoIguais(obj, obj2){
+function sãoIguais(obj, obj2) {
     return obj.inicio === obj2.inicio &&
-            obj.fim === obj2.fim &&
-            obj.atividade === obj2.atividade
+        obj.fim === obj2.fim &&
+        obj.atividade === obj2.atividade
 }
 
-function removeDaLista(obj){
-    let idx = atividades.findIndex(item => sãoIguais(item, obj))
-    if(idx > -1){
-        atividades.splice(idx, 1)
+function removeDaLista(obj, lista) {
+    let idx = lista.findIndex(item => sãoIguais(item, obj))
+    if (idx > -1) {
+        lista.splice(idx, 1)
         atualizaAtividadesStorage()
     }
 }
 
-function insereNoDOM(obj){
-    let tabela = document.querySelector("#listaAtividades")
+function insereNoDOM(obj, idTabDOM) {
+    let tabela = document.querySelector(idTabDOM)
     let linha = document.createElement("tr")
     let tdInicio = document.createElement("td")
     let tdFim = document.createElement("td")
@@ -111,26 +121,33 @@ function insereNoDOM(obj){
     linha.appendChild(tdAcoes)
 
     let btnExcluir = document.createElement("button")
-    let btnArquivar = document.createElement("button")
     tdAcoes.appendChild(btnExcluir)
-    tdAcoes.appendChild(btnArquivar)
-
     btnExcluir.innerHTML = "X"
-    btnArquivar.innerHTML = "OK"
 
-    btnExcluir.addEventListener("click", (evento)=>{
+    btnExcluir.addEventListener("click", (evento) => {
         let target = evento.target
-        let linhaTarget = target.parentNode.parentNode
+        let linhaTarget = target.parentNode //td
+                                .parentNode //tr
         tabela.removeChild(linhaTarget)
-        removeDaLista(obj)
-        
+        removeDaLista(obj, atividades)
+
     })
 
-    btnArquivar.addEventListener("click", (evento) => {
-        let target = evento.target
-        let linhaTarget = target.parentNode.parentNode
-        tabela.removeChild(linhaTarget)
-    })
+    if (idTabDOM !== "#atividadesConfirmadas") {
+        let btnArquivar = document.createElement("button")
+        btnArquivar.innerHTML = "OK"
+        tdAcoes.appendChild(btnArquivar)
+
+        btnArquivar.addEventListener("click", (evento) => {
+            let target = evento.target
+            let linhaTarget = target.parentNode.parentNode
+            tabela.removeChild(linhaTarget)
+            removeDaLista(obj, atividades)
+            insereNaLista(historico, obj)
+            insereNoDOM(obj, "#atividadesConfirmadas")
+
+        })
+    }
 
     tdInicio.innerHTML = obj.inicio
     tdFim.innerHTML = obj.fim
@@ -138,20 +155,20 @@ function insereNoDOM(obj){
     tdAtividades.innerHTML = obj.atividade
 }
 
-function insereNaLista(lista, obj)
-{
+function insereNaLista(lista, obj) {
     let existe = false
-    for(let item of lista){
-        if(sãoIguais(item, obj)){
+    for (let item of lista) {
+        if (sãoIguais(item, obj)) {
             existe = true
             break
         }
     }
-    if(!existe){
-        lista.push(obj)
+    if (!existe) {
+        lista.unshift(obj)
+        lista.splice(4, 1)
         atualizaAtividadesStorage()
         return true
-    }else{
+    } else {
         alert("Atividade já cadastrada previamente")
         return false
     }
